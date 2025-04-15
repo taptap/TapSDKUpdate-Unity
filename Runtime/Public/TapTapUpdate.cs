607e191797722ff0b071a6518ee74dc8dd045599
lc1006 <1176168734@qq.com>
2025-04-15 14:38:44 +0800

    feat:update upm

---
+++ Runtime/Public/TapTapUpdate.cs
@@ -0,0 +1,27 @@
+﻿using TapSDK.Update.Internal;
+using TapSDK.Core.Internal.Utils;
+using TapSDK.Core;
+using System;
+
+namespace TapSDK.Update {
+    public class TapTapUpdate {
+
+        public static readonly string Version = "4.6.1-beta.3";
+        
+        static readonly ITapUpdateBridge update;
+
+        static TapTapUpdate() {
+            update = BridgeUtils.CreateBridgeImplementation(typeof(ITapUpdateBridge), "TapSDK.Update")
+                as ITapUpdateBridge;
+        }
+
+        internal static void Init(string clientId, string clientToken)
+        {
+            update.Init(clientId, clientToken);
+        }
+
+        public static void UpdateGame(Action onCancel) {
+            update.UpdateGame(onCancel);
+        }
+    }
+}
